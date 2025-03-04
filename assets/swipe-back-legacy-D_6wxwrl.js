37f39385053ea7ec292ecdd7d8942ccb68db68ee
Aktziku <lnasnober@gmail.com>
2025-03-04 18:22:17 +0800

    Updates

--- assets/swipe-back-legacy-D_6wxwrl.js
+++ assets/swipe-back-legacy-D_6wxwrl.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CuR_jPgf.js","./index3-legacy-DKop_4ew.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CBtaNaHj.js","./index3-legacy-x09mJXfH.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
