babcced6284be15b7c8c6ec5cfb6edb46e7a91ad
Aktziku <lnasnober@gmail.com>
2025-04-30 19:57:53 +0800

    Updates

--- assets/md.transition-legacy-BNWUlJuB.js
+++ assets/md.transition-legacy-BNWUlJuB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5FrfaFw-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CJHPbFR-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
